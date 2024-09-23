a2a543dbcf2a8947395c319a97a65e16febb1a64
Awanish Mishra <awanishmishra@Awanishs-MacBook-Pro-2.local>
2024-09-24 02:05:09 +0530

    root file changed

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,22 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion"
 
-import Encapsulation from "./(root)/page";
+
+import HomeScreen from "@/components/Homescreen/HomeScreen";
+import Marquee from "react-fast-marquee";
+import MarqueeComponent from "@/components/Marquee/Marquee";
+import AboutPage from "./about/page";
+import ProjectPage from "./projects/page";
 
 
 export default function Home() {
   return (
-    <>
-    
-    <Encapsulation/>
-    
-  
-
-    </>
+   <main>
+    <HomeScreen />
+    <MarqueeComponent />
+    <AboutPage />
+    <ProjectPage />
+    <br /><br /><br />
+   </main>
   );
 }
